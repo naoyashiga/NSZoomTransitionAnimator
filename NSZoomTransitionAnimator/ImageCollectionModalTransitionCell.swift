00e7a6743eaf0e943274283290e6aa83d09fa13b
naoyashiga <naoyashiga0@gmail.com>
2015-06-25 22:00:45 +0900

    最初のコミット

---
+++ NSZoomTransitionAnimator/ImageCollectionModalTransitionCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCollectionModalTransitionCell.swift
+//  NSZoomTransitionAnimator
+//
+//  Created by naoyashiga on 2015/06/25.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionModalTransitionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ NSZoomTransitionAnimator/ImageCollectionModalTransitionViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ImageCollectionModalTransitionViewController.swift
+//  NSZoomTransitionAnimator
+//
+//  Created by naoyashiga on 2015/06/25.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier_ImageModalCell = "ImageModalCell"
+
+class ImageCollectionModalTransitionViewController: UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.collectionView?.registerNib(UINib(nibName: "ImageCollectionModalTransitionCell", bundle: nil), forCellWithReuseIdentifier: reuseIdentifier_ImageModalCell)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier_ImageModalCell, forIndexPath: indexPath) as! ImageCollectionModalTransitionCell
+        
+        cell.backgroundColor = UIColor.grayColor()
+    
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSize(width: self.view.bounds.width, height: self.view.bounds.height / 2.5)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 0.0
+    }
+    
+}
